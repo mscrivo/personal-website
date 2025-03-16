122b22078aecdff2954f85cb017a54c1135213a8
Michael Scrivo <mscrivo@gmail.com>
2025-03-16 17:36:24 -0400

    feat(assets): inject favicon

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), imagetools()],
+  plugins: [
+    react(),
+    imagetools(),
+    vitePluginFaviconsInject('./src/assets/logo/logo-bw.png'),
+  ],
 })
